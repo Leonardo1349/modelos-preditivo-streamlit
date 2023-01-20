44ce5b59d40157b3abdbaa277904be6ec68a8cb0
Leonardo Alves <leonardo.uern@hotmail.com>
2023-01-20 15:30:04 -0300

    Commit 1.16

--- streamlit/predicao_sem_historico_das_empresas/streamlit_app.py
+++ streamlit/predicao_sem_historico_das_empresas/streamlit_app.py
@@ -66,7 +66,7 @@ modelo_options = ['Extra Trees', 'Gradient Boosting', 'Random Forest']
 modelo_values = {'Extra Trees': modeloET, 'Gradient Boosting': modeloGB, 'Random Forest': modeloRF}
 
 
-st.header('Modelo de Predição de Sonegadores de Impostos Estaduais - Sem a Identificação da Empresa')
+st.header('Modelo de Predição de Sonegadores de Impostos Estaduais (Sem a Identificação da Empresa)')
 st.subheader('Preencha as informações solicitadas para obter a predição:')
 
 # Definindo os campos de entrada
